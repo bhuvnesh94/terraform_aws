a57857834ed9874372a4ebeedb67b90b86a030d5
bhuvnesh94 <bhuvnesh1510@gmail.com>
2020-01-03 18:06:13 +0530

    all entries in single file

--- instance.tf
+++ instance.tf
@@ -1,11 +1,10 @@
 provider "aws" {
-    access_key = "put your access key in this box"
-    secret_key = "put your secret key in this box"
-    region = "us-east-1"
-
+  access_key = "put your access key in this box"
+  secret_key = "put your secret key in this box"
+  region     = "us-east-1"
 }
+
 resource "aws_instance" "vm name here" {
-    instance_type = "t2.micro"
-    ami           = "ami-04b9e92b5572fa0d1"
+  instance_type = "t2.micro"
+  ami           = "ami-04b9e92b5572fa0d1"
 }
-
